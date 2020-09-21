f728206523026c8e6dda7ab3aa30e26323bc760d
jsp <jonsp14@gmail.com>
2020-09-21 16:45:10 -0700

    set up and center buttons/text

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var button1 = document.getElementById("buttons");
+button1.style.display="none";
+
+var test = document.querySelector("#checker");
+
+answer1.value.style.color="blue";
+
+// var button1 = document.getElementById("answer1");
+// var button1 = document.getElementById("answer1");
+// var button1 = document.getElementById("answer1");
+
+// On Start - initialize 1 minute timer - display top right corner
+
+// create/show 3 more buttons
+// 
+// change text of header ID = questions and buttons ID = answer1-4 to answers
+
+
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
