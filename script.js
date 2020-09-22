3438b8b6f2b759a4055c46900de2e2e80c1b4bda
jsp <jonsp14@gmail.com>
2020-09-21 19:51:26 -0700

    add timer, shuffle questions before posting to buttons

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
+var possible = [];
 
-var qa = {
-    questions: ["question 1", "question 2", "question 3"],
-    answers: ["answer 1", "answer 2", "answer 3", "answer 4", "answer 5", "answer 6", "answer 7", "answer 8", "answer 9",],
-    correct: ["correct 1", "correct 2", "correct 3"];
-};
+
+
+var questions = ["question 1", "question 2", "question 3"];
+var answers = ["answer 1", "answer 2", "answer 3", "answer 4", "answer 5", "answer 6", "answer 7", "answer 8", "answer 9",];
+var correct = ["correct 1", "correct 2", "correct 3"];
+
 var score = 0;
 
 
@@ -34,23 +36,50 @@ function start() {
     document.getElementById("checker").textContent = score;
 
     // start timer
-
-
 }
+
 // function to display question and answers
 function questionAnswer() {
+    var i = 0;
     do {
+        
+        
         let q = 0;
         let a = 0;
-        question.textContent = qa.questions[q];
-        answer1.textContent = qa.answers[a];
-        answer2.textContent = qa.answers[a + 1];
-        answer3.textContent = qa.answers[a + 2];
-        answer4.textContent = qa.correct[a + 3];
+        var x = Math.floor(Math.random() * 4);
+        possible.push(answers[q]);
+        possible.push(answers[q+1]);
+        possible.push(answers[q+2]);
+        possible.push(correct[a]);
+
+        console.log(possible);
+
+        // shuffle possible
+        for (let i = 0; i < possible.length; i++) {
+            var x = Math.floor(Math.random() * i);
+            var y = possible[i];
+            console.log("variable stored = " + y);
+            possible[i] = possible[x];
+            console.log("first swap = " + possible[i]);
+            possible[x] = y;
+            console.log("second swap = " + possible[x]);
+            console.log("password after swaps = " + possible);
+        }
+
+        console.log(possible);
+
+
+
+        question.textContent = questions[q];
+        answer1.textContent = possible[0];
+        answer2.textContent = possible[1];
+        answer3.textContent = possible[2];
+        answer4.textContent = possible[3];
+
         q = q + 1;
         a = a + 4;
-
-        
+        i = i + 1;
+        possible.length = 0;
 
 
         // if correct object is selected, score++, if incorrect, seconds - 5, then go to next question
@@ -62,25 +91,48 @@ function questionAnswer() {
 
 
 // function called when user clicks an answer button
-function answerClick() {
+// function answerClick() {
 
+// }
+
+// function startTimer() {
+//     seconds = 10;
+//     var timer = document.getElementById("timer");
+//     // We only want to start the timer if totalSeconds is > 0
+//     /* The "interval" variable here using "setInterval()" begins the recurring increment of the
+//        secondsElapsed variable which is used to check if the time is up */
+//     interval = setInterval(function () {
+//         seconds--;
+//         // So renderTime() is called here once every second.
+//         timer.innerHTML = seconds;
+//     }, 1000);
+//     if (seconds = 0) {
+//         return alert("Time is up!");
+//     }
+// }
+
+function timer() {
+    var seconds = 180;
+    function time() {
+        var timer = document.getElementById("timer");
+        seconds--;
+        timer.innerHTML = seconds;
+        if (seconds > 0) {
+            setTimeout(time, 1000);
+        } else {
+            alert("Time's up!");
+            finalScore();
+        }
+    }
+    time();
 }
 
 
-
-
-
-
-
-
-
-
-
 startQuiz.addEventListener("click", start);
-answer1.addEventListener("click", answerClick);
-answer2.addEventListener("click", answerClick);
-answer3.addEventListener("click", answerClick);
-answer4.addEventListener("click", answerClick);
+// answer1.addEventListener("click", answerClick);
+// answer2.addEventListener("click", answerClick);
+// answer3.addEventListener("click", answerClick);
+// answer4.addEventListener("click", answerClick);
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
