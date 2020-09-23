3dbbdc3812bc6e773d55c80202f0581d02da7d66
jsp <jonsp14@gmail.com>
2020-09-23 01:04:27 -0700

    add highscore and display functions, add input field and submit button

--- script2.js
+++ script2.js
@@ -102,28 +102,34 @@ const questions = [
     }];
 
 // attach variables to each id being used
-const displayStart = document.getElementById("buttons");
 const startQuiz = document.getElementById("start");
 const question = document.getElementById("questions");
+const text = document.getElementById("questionText");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const displayStart = document.getElementById("buttons");
 const answer1 = document.getElementById("1");
 const answer2 = document.getElementById("2");
 const answer3 = document.getElementById("3");
 const answer4 = document.getElementById("4");
-var text = document.getElementById("questionText");
-var seconds = 180;
+const checker = document.getElementById("checker");
 
-var answers = document.getElementsByClassName("answer");
-// console.log(answers);
+// Game time
+var seconds = 5;
+const answers = document.getElementsByClassName("answer");
+console.log(answers);
 
 
 // container for questions to display
 var container = [];
 var count = 0;
-// hide quiz buttons
+
+// hide quiz buttons and input
 displayStart.style.display = "none";
+input.style.display = "none";
 
 
-function start(check) {
+function start() {
     // show all buttons
     displayStart.style.display = "block";
     text.style.display = "none";
@@ -133,29 +139,29 @@ function start(check) {
 }
 
 function nextQuestion() {
-    if(seconds == 0){
-        alert("Time's up!");
+    if (seconds >= 1) {
+        display();
+    } else {
         highScore();
     }
-    if(count == 10){
-        alert("Good job!");
-        highScore();
-    }
-    display();
-
 }
 
 
 function display() {
-
+    var letters = ["a", "b", "c", "d"];
     var quesText = questions[count].question;
     question.textContent = quesText;
+    // // iterate through questions with index count
+    // questions[count][answers][answer index]
+    // answer1.innerText = questions[count]["answer"]["a"];
+    // answer2.innerText = questions[count]["answer"]["b"];
+    // answer3.innerText = questions[count]["answer"]["c"];
+    // answer4.innerText = questions[count]["answer"]["d"];
 
-    answer1.innerText = questions[count]["answer"]["a"];
-    answer2.innerText = questions[count]["answer"]["b"];
-    answer3.innerText = questions[count]["answer"]["c"];
-    answer4.innerText = questions[count]["answer"]["d"];
-
+    // test button text filler
+    for (i = 0; i < answers.length; i++) {
+        answers[i].innerText = questions[count]["answer"][letters[i]];
+    }
 }
 
 // set timeout in a recursive loop
@@ -165,12 +171,12 @@ function timer() {
         // timeElapsed++;
         seconds--;
         timer.innerHTML = seconds;
+        // stops timer at 0
         if (seconds > 0) {
-            // time it waits before running again
+            // set Timeout - time it waits before running again
             setTimeout(time, 1000);
         } else {
-            // alert("Time's up!");
-            return seconds;
+            return;
         }
     }
     // calls function again after completing
@@ -178,19 +184,25 @@ function timer() {
 }
 
 
-// iterate through array - list out all event handlers and return to start
+// iterate answer length - create event handlers = number of answers
 for (var i = 0; i < answers.length; i++) {
     answers[i].addEventListener("click", function (event) {
         event.preventDefault();
-        // check correct answer - add to counter - call next question
+        // targets clicked event - check if correct against question[count] correct answer - add to counter - call next question
         if (event.target.innerText == questions[count]["correct"]) {
             count++;
+            checker.textContent = "Correct!";
             nextQuestion();
             // console.log(event.target.innerText == questions[count]["correct"]);
         } else {
             // if wrong - subtract 5 seconds, add to count, call next question
+            checker.textContent = "Incorrect!";
             seconds = seconds - 5;
             count++;
+            if (seconds <= 0) {
+                alert("Time's up!");
+                highScore();
+            }
             nextQuestion();
             // console.log(event.target.innerText == questions[count]["correct"]);
         }
@@ -198,15 +210,48 @@ for (var i = 0; i < answers.length; i++) {
 }
 
 // function to display high scores - save to local storage
-function highScore(){
+function highScore() {
     displayStart.style.display = "none";
-    text.style.display = "block";
-    text.textContent = "Congratulations! Here are the high scores: ";
-    question.textContent = "High Scores";
-    var score = seconds;
 
+    // question.style.display = "none";
+    if (seconds > 0) {
+        text.style.display = "block";
+        text.textContent = "Congratulations! Please enter your name: ";
+    } else {
+        text.style.display = "block";
+        text.textContent = "Nice try! Please Enter your name: ";
+    }
+    
+    question.textContent = "Game Over";
+    input.style.display = "block";
+    var score = seconds;
+    console.log(name);
+    localStorage.setItem("score", score);
+    var name = input.textContent;
+    // error checking for name
+    if (name === "" || name === null) {
+        alert("You must enter at least one character to save your score.");
+        highScore();
+    } else {
+        localStorage.setItem("name", name);
+        displayScore();
+    }
+}
+
+// create display score function
+function displayScore() {
+    
+    var create = document.createElement("div");
+    text.appendChild(create);
+    // create.setAttribute("style", "text-align: center; font-size: 50px;");
+    // var winners = JSON.parse(localStorage.getItem("name", "score"));
+    // console.log(winners);
+    create.textContent = "1st Place: " + name + " Score: " + score;
 }
 
 
 
+// event listener for start button
 startQuiz.addEventListener("click", start);
+// event handler for submit button
+submit.addEventListener("submit", displayScore);
